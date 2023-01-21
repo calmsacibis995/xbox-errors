9ae3d046980fb11acb3a4168efe2b9452c087a81
Stefanos Stefanidis <46319359+calmsacibis995@users.noreply.github.com>
2023-01-21 09:10:00 +0200

    Add numbers in decimal.

--- errors.h
+++ errors.h
@@ -1,23 +1,25 @@
-#define FATAL_ERROR_NONE                0x00
-#define FATAL_ERROR_CORE_DIGITAL        0x01
-#define FATAL_ERROR_BAD_EEPROM          0x02
-#define FATAL_ERROR_UNUSED1             0x03    
-#define FATAL_ERROR_BAD_RAM             0x04
-#define FATAL_ERROR_HDD_NOT_LOCKED      0x05
-#define FATAL_ERROR_HDD_CANNOT_UNLOCK   0x06
-#define FATAL_ERROR_HDD_TIMEOUT         0x07
-#define FATAL_ERROR_HDD_NOT_FOUND       0x08
-#define FATAL_ERROR_HDD_BAD_CONFIG      0x09
-#define FATAL_ERROR_DVD_TIMEOUT         0x0A
-#define FATAL_ERROR_DVD_NOT_FOUND       0x0B
-#define FATAL_ERROR_DVD_BAD_CONFIG      0x0C
-#define FATAL_ERROR_XBE_DASH_GENERIC    0x0D
-#define FATAL_ERROR_XBE_DASH_ERROR      0x0E
-#define FATAL_ERROR_UNUSED2             0x0F
-#define FATAL_ERROR_XBE_DASH_SETTINGS   0x10
-#define FATAL_ERROR_UNUSED3             0x11
-#define FATAL_ERROR_UNUSED4             0x12
-#define FATAL_ERROR_UNUSED5             0x13
-#define FATAL_ERROR_XBE_DASH_X2_PASS    0x14
-#define FATAL_ERROR_REBOOT_ROUTINE      0x15
-#define FATAL_ERROR_RESERVED            0xFF
+// See the wiki of this repository for more information.
+// Taken from the Xbox sources.
+#define FATAL_ERROR_NONE                0x00      // 00
+#define FATAL_ERROR_CORE_DIGITAL        0x01      // 01
+#define FATAL_ERROR_BAD_EEPROM          0x02      // 02
+#define FATAL_ERROR_UNUSED1             0x03      // 03
+#define FATAL_ERROR_BAD_RAM             0x04      // 04
+#define FATAL_ERROR_HDD_NOT_LOCKED      0x05      // 05
+#define FATAL_ERROR_HDD_CANNOT_UNLOCK   0x06      // 06
+#define FATAL_ERROR_HDD_TIMEOUT         0x07      // 07
+#define FATAL_ERROR_HDD_NOT_FOUND       0x08      // 08
+#define FATAL_ERROR_HDD_BAD_CONFIG      0x09      // 09
+#define FATAL_ERROR_DVD_TIMEOUT         0x0A      // 10
+#define FATAL_ERROR_DVD_NOT_FOUND       0x0B      // 11
+#define FATAL_ERROR_DVD_BAD_CONFIG      0x0C      // 12
+#define FATAL_ERROR_XBE_DASH_GENERIC    0x0D      // 13
+#define FATAL_ERROR_XBE_DASH_ERROR      0x0E      // 14
+#define FATAL_ERROR_UNUSED2             0x0F      // 15
+#define FATAL_ERROR_XBE_DASH_SETTINGS   0x10      // 16
+#define FATAL_ERROR_UNUSED3             0x11      // 17
+#define FATAL_ERROR_UNUSED4             0x12      // 18
+#define FATAL_ERROR_UNUSED5             0x13      // 19
+#define FATAL_ERROR_XBE_DASH_X2_PASS    0x14      // 20
+#define FATAL_ERROR_REBOOT_ROUTINE      0x15      // 21
+#define FATAL_ERROR_RESERVED            0xFF      // 255
